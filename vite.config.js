f6276e8ca49caceb16d32c1d1cdf3ba8f5618b82
David Vazquez <112228738+DaveOval@users.noreply.github.com>
2023-04-26 16:28:57 -0600

    se modifico el archivo vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+  build:{
+    outDir: 'docs',
+  },
   plugins: [react()],
 })
