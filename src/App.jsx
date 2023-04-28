f3ec402cbaeec4d17c984cc502849d5142465d2a
Alvaro Gonzalez <alvarodavidgo848@gmail.com>
2023-04-27 22:52:45 -0500

    agregar paquete react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Boton from './components/boton'
 import Logo from './components/logo'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 function App() {
   
