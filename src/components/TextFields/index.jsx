4739fa238df62aeb0b5b9642ec8d61595748a9b8
Edgar Reyes <edyreyes2305@gmail.com>
2023-05-10 10:19:26 -0500

    Added component textfields

---
+++ src/components/TextFields/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../TextFields/textfield.css'
+import { TextField } from '@mui/material';
+export const TextFields = () => {
+  return (
+    <>
+        <TextField id="standard-basic" label="Standard" variant="standard" />
+    </>
+  )
+}
