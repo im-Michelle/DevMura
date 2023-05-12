2e097547e60f828bca3587812a717b7d381c10ef
Michelle Trejo <michelletgar@gmail.com>
2023-05-11 20:41:17 -0600

    ci: data post

---
+++ src/data/posts.js
@@ -0,0 +1,147 @@
+export const posts = () => [
+    {
+        key: 1,
+        name: 'Juanito Banana',
+        role: 'Frontend Developer',
+        userName: "juanito6000",
+        time: "hace 1 hora",
+        img: "https://images.pexels.com/photos/4275717/pexels-photo-4275717.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        bodyText: "Me gusta el stack MERN"
+
+    },
+    {
+        key: 2,
+        name: 'Mariana Smith',
+        role: 'Full Stack Developer',
+        userName: "mariana_dev",
+        time: "hace 30 minutos",
+        img: "https://images.pexels.com/photos/806835/pexels-photo-806835.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        bodyText: "¡Disfrutando de un día de desarrollo web! #codinglife"
+    },
+    {
+        key: 3,
+        name: 'Alex Turner',
+        role: 'Backend Engineer',
+        userName: "alex_t",
+        time: "hace 2 horas",
+        img: "https://images.pexels.com/photos/936119/pexels-photo-936119.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        bodyText: "Trabajando en la optimización de consultas SQL. #database"
+    },
+    {
+        key: 4,
+        name: 'Sophie Chen',
+        role: 'UI/UX Designer',
+        userName: "sophie.design",
+        time: "hace 45 minutos",
+        img: "https://images.pexels.com/photos/594421/pexels-photo-594421.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        bodyText: "Explorando nuevas tendencias de diseño para aplicaciones móviles."
+    },
+    {
+        key: 5,
+        name: 'Raj Patel',
+        role: 'Software Engineer',
+        userName: "raj.codes",
+        time: "hace 1 hora",
+        img: "https://images.pexels.com/photos/718978/pexels-photo-718978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        bodyText: "Compartiendo mi experiencia con el framework Angular. #angular"
+    },
+    {
+        key: 6,
+        name: 'Emily Thompson',
+        role: 'Frontend Developer',
+        userName: "emily.dev",
+        time: "hace 25 minutos",
+        img: "https://images.pexels.com/photos/774095/pexels-photo-774095.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        bodyText: "¡JavaScript es mi lenguaje favorito para el desarrollo web!"
+    },
+    {
+        key: 7,
+        name: 'Michael Wilson',
+        role: 'DevOps Engineer',
+        userName: "michael.ops",
+        time: "hace 3 horas",
+        img: "https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        bodyText: "Automatizando despliegues con Jenkins y Docker. #devops"
+    },
+    {
+        key: 8,
+        name: 'Ana Torres',
+        role: 'Frontend Developer',
+        userName: "ana_torres",
+        time: "hace 6 horas",
+        img: "https://images.pexels.com/photos/1587009/pexels-photo-1587009.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        bodyText: "¿Cuál es su framework de CSS favorito?"
+
+    },
+    {
+        key: 9,
+        name: 'Andrés García',
+        role: 'Full-stack Developer',
+        userName: "andres_garcia",
+        time: "hace 5 horas",
+        img: "https://images.pexels.com/photos/1680175/pexels-photo-1680175.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        bodyText: "Estoy trabajando en un proyecto de Django."
+
+    },
+    {
+        key: 10,
+        name: 'Pablo Jiménez',
+        role: 'Backend Developer',
+        userName: "pablo_jimenez",
+        time: "hace 3 horas",
+        img: "https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        bodyText: "Estoy trabajando en un proyecto de Node.js y Express."
+
+    },
+    {
+        key: 11,
+        name: 'Pedro Hernández',
+        role: 'Backend Developer',
+        userName: "pedro_hernandez",
+        time: "hace 7 horas",
+        img: "https://images.pexels.com/photos/1516680/pexels-photo-1516680.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        bodyText: "Estoy trabajando en un proyecto de Flask."
+
+    },
+    {
+        key: 12,
+        name: 'María Pythonista',
+        role: 'Data Scientist',
+        userName: "mariapython",
+        time: "hace 10 minutos",
+        img: "https://images.pexels.com/photos/1181686/pexels-photo-1181686.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        bodyText: "¡Inmersa en un nuevo proyecto de análisis de datos con Python! 🐍📊 #DataScience"
+    },
+    {
+        key: 13,
+        name: 'Carlos CodeMaster',
+        role: 'Full Stack Developer',
+        userName: "ccodemaster",
+        time: "hace 45 minutos",
+        img: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        bodyText: "¡Emocionado por empezar un nuevo proyecto con el stack MERN! 💻🚀"
+    },
+    {
+        key: 14,
+        name: 'Pablo CSSWizard',
+        role: 'UI/UX Designer',
+        userName: "pablocss",
+        time: "hace 20 minutos",
+        img: "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        bodyText: "¿Alguien más aquí apasionado por el diseño web? Compartan sus proyectos 🎨💻"
+    },
+    {
+        key: 15,
+        name: 'Ana ByteQueen',
+        role: 'Backend Developer',
+        userName: "byteQueenAna",
+        time: "hace 2 horas",
+        img: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        bodyText: "Trabajando en optimizar el rendimiento de mi API REST"
+    }
+
+]
+
+
+
+// key, name, role, userName, time, img, bodyText
