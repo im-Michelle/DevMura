c1fa9e1103bf01a6d3fa5d02998922cf8088355b
goyo2205 <jgregorio.ef@gmail.com>
2023-05-11 19:50:10 -0600

    ci: se agrega la pagina de profile en blanco

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
 import './Button.css'
 import React from 'react'
 
 const CustomButton = ({onClick, children, backgrdColor}) =>{
     return(
         <>
-            <button className='btnCustom' onClick={onClick} style={{backgroundColor: backgrdColor}}>{children}</button>
+            <Link to='/sign-in' className='btnCustom' onClick={onClick} style={{backgroundColor: backgrdColor}}>{children}</Link>
+            {/* <button className='btnCustom' onClick={onClick} style={{backgroundColor: backgrdColor}}>{children}</button> */}
         </>
     )
 
---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavbarDefault from "../../components/Navbar-Default";
+
+
+
+const ProfilePage = () => {
+  return (
+    <>
+        <NavbarDefault/>
+
+    </>
+  )
+}
+
+export default ProfilePage;
