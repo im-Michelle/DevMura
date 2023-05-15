fad9fdfa77eda0a2295fc522e77f84ed8f9d02e0
goyo2205 <jgregorio.ef@gmail.com>
2023-05-15 17:50:39 -0600

    se genera mi rama

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -6,19 +6,14 @@ import { colors } from '../../ui/colors';
 import styled from 'styled-components';
 import { NavBarFeed } from '../../components/Navbar-feed';
 
-const Main = styled.div`
-    border: 3px solid purple;
+const Main = styled.main`
+    padding-top: 5rem;
 `;
-const ContainerForNAvBAr =styled.div`
-    margin-bottom: 5em;
-    border: .001px solid black;
-`;
+
 const ProfilePage = () => {
   return (
     <>
-        <ContainerForNAvBAr>
-            <NavBarFeed/>
-        </ContainerForNAvBAr>
+        <NavBarFeed/>
         <Main>
             <Header 
                 name="Susana"
@@ -27,6 +22,12 @@ const ProfilePage = () => {
                 img="https://images.pexels.com/photos/7841717/pexels-photo-7841717.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                 />
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro, at eligendi inventore amet soluta officiis aspernatur asperiores harum accusamus, iusto veniam reiciendis ex a aliquam provident explicabo, culpa qui! Veniam.
+                Consectetur vitae quod quo velit nulla dolor dicta illo accusamus modi nihil explicabo cumque corrupti sed labore quis ea, temporibus, animi rem iusto natus, dolorum sit! Corrupti voluptatibus illo voluptas?
+                Ab labore asperiores velit ad consequuntur quaerat sed aut doloribus quasi? Assumenda ipsam hic incidunt eius architecto culpa consectetur voluptas dolor, nisi ipsa animi ea, quas illo quisquam reiciendis voluptate.
+               eaque minus! Obcaecati accusantium id sint molestias inventore ullam laboriosam blanditiis numquam eius. Quas provident rem id beatae obcaecati deserunt voluptatem iusto neque. Sequi, inventore? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro, at eligendi inventore amet soluta officiis aspernatur asperiores harum accusamus, iusto veniam reiciendis ex a aliquam provident explicabo, culpa qui! Veniam.
+                Consectetur vitae quod quo velit nulla dolor dicta illo accusamus modi nihil explicabo cumque corrupti sed labore quis ea, temporibus, animi rem iusto natus, dolorum sit! Corrupti voluptatibus illo voluptas?
+                Ab labore asperiores velit ad consequuntur quaerat sed aut doloribus quasi? Assumenda ipsam hic incidunt eius architecto culpa consectetur voluptas dolor, nisi ipsa animi ea, quas illo quisquam reiciendis voluptate.
+               eaque minus! Obcaecati accusantium id sint molestias inventore ullam laboriosam blanditiis numquam eius. Quas provident rem id beatae obcaecati deserunt voluptatem iusto neque. Sequi, inventore? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro, at eligendi inventore amet soluta officiis aspernatur asperiores harum accusamus, iusto veniam reiciendis ex a aliquam provident explicabo, culpa qui! Veniam.
                 Consectetur vitae quod quo velit nulla dolor dicta illo accusamus modi nihil explicabo cumque corrupti sed labore quis ea, temporibus, animi rem iusto natus, dolorum sit! Corrupti voluptatibus illo voluptas?
                 Ab labore asperiores velit ad consequuntur quaerat sed aut doloribus quasi? Assumenda ipsam hic incidunt eius architecto culpa consectetur voluptas dolor, nisi ipsa animi ea, quas illo quisquam reiciendis voluptate.
                eaque minus! Obcaecati accusantium id sint molestias inventore ullam laboriosam blanditiis numquam eius. Quas provident rem id beatae obcaecati deserunt voluptatem iusto neque. Sequi, inventore?
