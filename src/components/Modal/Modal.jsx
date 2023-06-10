dc4cc5832ca5558bf47af8f0b987ba6626b4fa7c
Tenshik0 <tenshik0.ale@gmail.com>
2023-06-10 01:32:29 -0600

    Modal textField
    TextField in modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,41 +1,91 @@
 import React, { useState } from 'react';
 import Modal from '@mui/material/Modal';
-import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { styled } from '@mui/system';
 import { colors } from "../../ui/colors";
 import "./modal.css";
 
+const TextFieldStyled = styled(TextField)`
+  input {
+    color: ${colors.primaryText};
+  }
+  label {
+    color: ${colors.primaryText};
+  }
+  .MuiInput-underline:before {
+    border-bottom-color: ${colors.navy};
+  }
 
+  .MuiInput-underline:hover:not(.Mui-disabled):before {
+    border-bottom-color: ${colors.vibrantBlue};
+  }
+
+  .MuiInput-underline:after {
+    border-bottom-color: ${colors.primaryText};
+  }
+  .MuiFormHelperText-root {
+    color: ${colors.secondaryText};
+  }
+  .MuiFormLabel-root.Mui-error {
+    color: ${colors.contrast};
+  }
+  .MuiInputLabel-root {
+    color: ${colors.primaryText};
+  }
+
+  .MuiInputLabel-root.Mui-focused {
+    color: ${colors.lightBlue};
+  }
+`;
 
 const ModalProfile = ({ open, onClose }) => {
-    return (
-        <Modal
-          open={open}
-          onClose={onClose}
-          aria-labelledby="parent-modal-title"
-          aria-describedby="parent-modal-description"
-        >
-          <Box 
-            sx={{ 
-                position: 'absolute', 
-                top: '50%', 
-                left: '50%', 
-                transform: 'translate(-50%, -50%)', 
-                bgcolor: `${colors.new}`, 
-                border: '2px solid #000', 
-                boxShadow: 24, 
-                p: 4 
-              }}
-            >
-            
-            <div id='title'>
-                <h3 id="parent-modal-title"  style={{color: `${colors.lightBlue}`}}> Edit profile </h3>
-            </div>
-            <p id="parent-modal-description" style={{color: `${colors.lightBlue}`}}> Basic Information </p>
-          </Box>
-        
-        </Modal>
-      );
+  const [name, setName] = useState('');
+  const [isValidName, setIsValidName] = useState(true);
+
+  const handleInputNameChange = (e) => {
+    const inputValue = e.target.value;
+    const regeName = /^[A-Za-záéíóúüñÁÉÍÓÚÜÑ]+(?: [A-Za-záéíóúüñÁÉÍÓÚÜÑ]+)*$/;
+    if (
+      inputValue.length < 3 ||
+      inputValue.length > 50 ||
+      !inputValue.match(regeName)
+    ) {
+      setIsValidName(false);
+    } else {
+      setIsValidName(true);
+    }
+    setName(inputValue);
+  };
+
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      aria-labelledby="parent-modal-title"
+      aria-describedby="parent-modal-description"
+    >
+      <Box className="modal-container"  sx={{p: 4,}}>
+        <div id='title'>
+          <h3 id="parent-modal-title" style={{ color: colors.lightBlue }}>Edit profile</h3>
+        </div>
+        <p id="parent-modal-description" style={{ color: colors.lightBlue }}>Basic Information</p>
+
+        <TextFieldStyled
+          id="name"
+          type="text"
+          label="Name"
+          variant="standard"
+          value={name}
+          helperText={isValidName ? '' : 'Invalid name'}
+          error={!isValidName}
+          required
+          inputProps={{ style: { textTransform: 'capitalize' } }}
+          onChange={handleInputNameChange}
+        />
+      </Box>
+    </Modal>
+  );
 };
 
 export default ModalProfile;
