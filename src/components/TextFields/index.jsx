893f0ee88e332169f27612080952c58a47e0b037
Tenshik0 <tenshik0.ale@gmail.com>
2023-06-10 02:15:16 -0600

    componente textField
    TextField

--- src/components/TextFields/index.jsx
+++ src/components/TextFields/index.jsx
@@ -1,33 +1,54 @@
-import React, { useState } from 'react';
-import '../TextFields/textfield.css';
-import { TextField } from '@mui/material';
-import Grid from '@mui/material/Unstable_Grid2';
+import React from "react";
+import styled from "@emotion/styled";
+import { colors } from "../../ui/colors";
+import TextField from "@mui/material/TextField";
+import { IconButton, InputAdornment } from "@mui/material";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import "./textfield.css"
 
-const TextFields = ({ typeInput, helper, label }) => {
+const TextFieldStyled = styled(TextField)`
+  input {
+    color: ${colors.primaryText};
+  }
+  label {
+    color: ${colors.primaryText};
+  }
+  .MuiInput-underline:before {
+    border-bottom-color: ${colors.navy}; 
+  }
+  .MuiInput-underline:hover:not(.Mui-disabled):before {
+    border-bottom-color: ${colors.vibrantBlue}; 
+  }
+  .MuiInput-underline:after {
+    border-bottom-color: ${colors.primaryText}; 
+  }
+  .MuiFormHelperText-root {
+    color: ${colors.secondaryText}; 
+  }
+  .MuiFormLabel-root.Mui-error {
+    color: ${colors.contrast}; 
+  }
+  .MuiInputLabel-root {
+    color: ${colors.vibrantBlue};
+  }
+  .MuiInputLabel-root.Mui-focused {
+    color: ${colors.lightBlue}; 
+  }
+`;
 
+const TextFields = (props) => {
+  const { showPassword, setShowPassword, ...rest } = props;
 
+  const inputAdornment = (
+    <InputAdornment position="end">
+      <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
+        {showPassword ? <VisibilityOff /> : <Visibility />}
+      </IconButton>
+    </InputAdornment>
+  );
 
-
-  return (
-    <>
-      
-      <Grid container spacing={3}>
-        <Grid xs={12} md={10} xl={10} className="textfieldGrid" >
-          <TextField
-            id="filled-error-helper-text"
-            type={typeInput}
-            label={label}
-            variant="filled"
-            helperText={helper}
-            fullWidth
-            required
-            className="textfields"
-            style={{marginTop: 30, marginBottom: "auto"}}
-          />
-        </Grid>
-      </Grid>
-    </>
-  )
-}
+  return <TextFieldStyled {...rest} InputProps={rest.type === "password" ? { endAdornment: inputAdornment } : {}} />;
+};
 
 export default TextFields;
