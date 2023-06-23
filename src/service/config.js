40d3ea4553fb3fa85e2f51bfb7fd089f0d3e1348
Tenshik0 <tenshik0.ale@gmail.com>
2023-06-23 08:35:01 -0600

    Update config.js

--- src/service/config.js
+++ src/service/config.js
@@ -1,7 +1,7 @@
 //*  URL base para las peticiones de la api
 /* export const BASE_URL = "http://localhost:8080/api";
 export const BASE_URL_LOGIN = "http://localhost:8080/"; */
-export const BASE_URL = "https://devmuraback.up.railway.app/api";
-export const BASE_URL_LOGIN = "https://devmuraback.up.railway.app/";
-/* export const BASE_URL = "https://web-production-a15a.up.railway.app/api";
-export const BASE_URL_LOGIN = "https://web-production-a15a.up.railway.app/"; */
+/* export const BASE_URL = "https://devmuraback.up.railway.app/api";
+export const BASE_URL_LOGIN = "https://devmuraback.up.railway.app/"; */
+export const BASE_URL = "https://web-production-a15a.up.railway.app/api";
+export const BASE_URL_LOGIN = "https://web-production-a15a.up.railway.app/";
